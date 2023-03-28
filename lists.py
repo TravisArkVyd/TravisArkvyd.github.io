d8146572be5228d7faa27a83e0cf20f73f6696ae
TravisArkVyd <travis@arkvyd.com>
2023-03-27 21:16:05 -0400

    learning lists

---
+++ lists.py
@@ -0,0 +1,19 @@
+# define a list of names
+names = ["Harry", "Ron", "Hermoine", "Ginny"]
+
+print(names[0])
+
+names.append("Draco")
+
+print(names)
+
+names.sort()
+
+print(names)
+
+
+feelings = ["overwhelmed", "burnt-out"]
+
+feelings.append("tired")
+
+print(feelings)
